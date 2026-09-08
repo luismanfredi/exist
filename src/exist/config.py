e75383e993aa7a8cc0b59f4c64893ab3151b06a2
luismanfredi <luismanfredi920@gmail.com>
2026-09-07 21:56:16 -0300

    data: finish data cleaning

--- src/exist/config.py
+++ src/exist/config.py
@@ -3,4 +3,4 @@ from pathlib import Path
 DATA_DIR = Path(__file__).resolve().parents[2] / "data"
 
 DATA_PATH = DATA_DIR / "raw" / "star_classification.csv"
-output_data_dir = DATA_DIR / "processed"
+output_data_dir = DATA_DIR / "processed" / "processed_star_classification.parquet"
