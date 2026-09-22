4f14d31c131c0eb8008bfe532948e8115bec86af
luismanfredi <luismanfredi920@gmail.com>
2026-09-22 16:00:06 -0300

    feat: add postgres databse integration with the api

--- src/exist/api/main.py
+++ src/exist/api/main.py
@@ -1,3 +1,4 @@
+import logging
 from contextlib import asynccontextmanager
 
 import joblib
@@ -5,7 +6,10 @@ from fastapi import FastAPI, HTTPException
 
 from exist.api.schemas import ObjectFeatures, ObjectPrediction
 from exist.config import MODEL_DIR
+from exist.db.db import log_prediction
 from exist.utils import complete_table
+
+logger = logging.getLogger(__name__)
 
 
 @asynccontextmanager
@@ -26,5 +30,11 @@ def predict(features: ObjectFeatures) -> ObjectPrediction:
         proba = model.predict_proba(values)[0].max()
     except Exception as e:  # noqa BLE001
         raise HTTPException(status_code=500, detail=f"prediction error: {e}")
+
+    try:
+        converted_data = {str(k): float(v) for k, v in features.model_dump().items()}
+        log_prediction(converted_data, pred, proba, "Random_Forest_V1")
+    except Exception:
+        logger.exception("Failed to log prediction")
 
     return ObjectPrediction(predicted_class=str(pred), confidence=float(proba))
